5bebcb801de508f36b3ad41535d83bdd58b20aa7
곽범석 <qjatjr99@naver.com>
2023-11-14 17:34:02 +0900

    BGP-57 feat: Transition Animation 구현

---
+++ frontend/src/components/transition/animation.tsx
@@ -0,0 +1,65 @@
+export type AnimationObject = [Keyframe[], KeyframeAnimationOptions]
+
+export const rightFadeinAnimation = [
+    [
+        { opacity: '0', transform: 'translateX(10px)', visibility: 'hidden' },
+        { opacity: '1', transform: 'translateX(0px)', visibility: 'visible' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
+
+export const rightFadeoutAnimation = [
+    [
+        { opacity: '1', transform: 'translateX(0px)', visibility: 'visible' },
+        { opacity: '0', transform: 'translateX(-10px)', visibility: 'hidden' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
+
+export const leftFadeinAnimation = [
+    [
+        { opacity: '0', transform: 'translateX(-10px)', visibility: 'hidden' },
+        { opacity: '1', transform: 'translateX(0px)', visibility: 'visible' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
+
+export const leftFadeoutAnimation = [
+    [
+        { opacity: '1', transform: 'translateX(0px)', visibility: 'visible' },
+        { opacity: '0', transform: 'translateX(10px)', visibility: 'hidden' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
+
+export const bottomFadeinAnimation = [
+    [
+        { opacity: '0', transform: 'translateY(10px)', visibility: 'hidden' },
+        { opacity: '1', transform: 'translateY(0px)', visibility: 'visible' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
+
+export const bottomFadeoutAnimation = [
+    [
+        { opacity: '1', transform: 'translateY(0px)', visibility: 'visible' },
+        { opacity: '0', transform: 'translateY(-10px)', visibility: 'hidden' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
+
+export const topFadeinAnimation = [
+    [
+        { opacity: '0', transform: 'translateY(-10px)', visibility: 'hidden' },
+        { opacity: '1', transform: 'translateY(0px)', visibility: 'visible' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
+
+export const topFadeoutAnimation = [
+    [
+        { opacity: '1', transform: 'translateY(0px)', visibility: 'visible' },
+        { opacity: '0', transform: 'translateY(10px)', visibility: 'hidden' },
+    ],
+    { duration: 200, easing: 'ease-out', fill: 'forwards' },
+] as AnimationObject
