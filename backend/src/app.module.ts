f10098f3d3a7f5f57f68032c45b4a22ae321827e
jhhwang <101859033+ttobe@users.noreply.github.com>
2023-11-14 11:14:09 +0900

    BGP-17 feat-be: auth 컴포넌트 생성
    nest g res auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
