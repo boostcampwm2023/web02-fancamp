a069cb44c20ff784c76bbf36940d79e09c2b23c0
Yeong <yjo0723@hotmail.com>
2023-11-14 19:56:34 +0900

    feat-fe: 리액트 라우터 라우팅 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,33 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './utils/router'
-import './styles/index.css'
+import ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import './styles/index.css';
+import App from './App';
+import SigninPage from './pages/auth/signin';
+import DemoPage from './pages/components/demo/demoPage';
+import SignupPage from './pages/auth/signup';
+import ChatPage from './pages/camps/chat';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App />} />
+      <Route path='/auth'>
+        <Route path='signin' element={<SigninPage />} />
+        <Route path='signup' element={<SignupPage />} />
+      </Route>
+      <Route path='/camps/:campId'>
+        <Route path='chat' element={<ChatPage />} />
+      </Route>
+      <Route path='/components/demo' element={<DemoPage />} />
+    </>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,36 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            borderRadius: {
-                sm: '0.25rem', // 4px
-                md: '0.5rem', // 8px
-                lg: '1rem', // 16px
-            },
-            borderWidth: {
-                sm: '0.0625rem', // 1px
-                md: '0.125rem', // 2px
-                lg: '0.25rem', // 4px
-            },
-        },
-        spacing: {
-            sm: '0.5rem', // 8px
-            md: '1rem', // 16px
-            lg: '1.5rem', // 24px
-            xl: '2rem', // 32px
-        },
-        colors: {
-            'text-primary': '#111111',
-            'text-secondary': '#777777',
-            'contour-primary': '#DFDFDF',
-            'point-green': '#ADFF00',
-            'point-yellow': '#FFC044',
-            'point-blue': '#0087E9',
-            'point-lavender': '#D4AAFF',
-            transparent: 'transparent',
-            'surface-primary': '#FFFFFF',
-        },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      borderRadius: {
+        sm: '0.25rem', // 4px
+        md: '0.5rem', // 8px
+        lg: '1rem', // 16px
+      },
+      borderWidth: {
+        sm: '0.0625rem', // 1px
+        md: '0.125rem', // 2px
+        lg: '0.25rem', // 4px
+      },
     },
-    plugins: [],
-}
+    spacing: {
+      xs: '0.25rem', // 4px
+      sm: '0.5rem', // 8px
+      md: '1rem', // 16px
+      lg: '1.5rem', // 24px
+      xl: '2rem', // 32px
+    },
+    colors: {
+      'text-primary': '#111111',
+      'text-secondary': '#777777',
+      'contour-primary': '#DFDFDF',
+      'point-green': '#ADFF00',
+      'point-yellow': '#FFC044',
+      'point-blue': '#0087E9',
+      'point-lavender': '#D4AAFF',
+      transparent: 'transparent',
+      'surface-primary': '#FFFFFF',
+    },
+  },
+  plugins: [],
+};
