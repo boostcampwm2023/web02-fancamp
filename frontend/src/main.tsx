f5a8253351392dce31a30748ecd66529e8f0cf40
곽범석 <qjatjr99@naver.com>
2023-11-14 17:39:43 +0900

    BGP-60 feat: 컴포넌트 시연을 위한 데모페이지 구현

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './utils/router'
+import './styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
