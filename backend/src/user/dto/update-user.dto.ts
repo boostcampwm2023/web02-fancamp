c3d792cd386a6759e59ea9795a83ebccc7fc5d0a
Jiwon-Heo <58136348+hunnypooh@users.noreply.github.com>
2023-11-14 22:14:14 +0900

    BGP-17 refactor: user 모듈 정리
    1. 사용 안하는 코드 주석처리.
    2. create-user dto 삭제.

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { CreateUserAuthDto } from 'src/auth/dto/create-auth.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserAuthDto) {}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRepository } from './user.repository';
 import { CreateUserAuthDto } from 'src/auth/dto/create-auth.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository){}
-  create(createUserAuthDto: CreateUserAuthDto) {
-    return this.userRepository.createUser(createUserAuthDto)
-  }
+  constructor(private readonly userRepository: UserRepository) {}
 
   findAll() {
     return `This action returns all user`;
@@ -19,11 +15,11 @@ export class UserService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
