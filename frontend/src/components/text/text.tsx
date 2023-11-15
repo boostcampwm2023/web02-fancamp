9a9621b6308cc214da86fe5a6f79d476ec37691b
곽범석 <qjatjr99@naver.com>
2023-11-15 21:50:30 +0900

    BGP-75 feat-fe: Text 컴포넌트 구현

---
+++ frontend/src/components/text/text.tsx
@@ -0,0 +1,31 @@
+import { colors } from '../../utils/constants'
+
+interface TextProps {
+  children: React.ReactNode
+  size?: 12 | 14 | 20
+  color?: keyof typeof colors
+}
+
+const Text = ({ children, size = 14, color = 'text-primary' }: TextProps) => {
+  const fontSizes = {
+    12: 'display-regular-12',
+    14: 'display-regular-14',
+    20: 'display-regular-20',
+  }
+
+  const fontColors = {
+    transparent: 'text-transparent',
+    'text-primary': 'text-text-primary',
+    'text-secondary': 'text-text-secondary',
+    'contour-primary': 'text-contour-primary',
+    'point-green': 'text-point-green',
+    'point-yellow': 'text-point-yellow',
+    'point-blue': 'text-point-blue',
+    'point-lavender': 'text-point-lavender',
+    'surface-primary': 'text-surface-primary',
+  }
+
+  return <span className={`${fontSizes[size]} ${fontColors[color]} test`}>{children}</span>
+}
+
+export default Text
