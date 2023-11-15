279e63204ce54a3957f5eda6dacb0d8e0dafafad
곽범석 <qjatjr99@naver.com>
2023-11-15 21:47:37 +0900

    BGP-71 feat-fe: Dropdown 컴포넌트 구현

---
+++ frontend/src/components/dropdown/dropdownItem.tsx
@@ -0,0 +1,23 @@
+import Text from '../text/text'
+
+interface DropdownItemProps {
+  text: string
+  icon?: React.ReactNode
+  handleOnClick: (text: string) => void
+}
+
+const DropdownItem = ({ text, icon, handleOnClick }: DropdownItemProps) => {
+  return (
+    <li
+      className="h-[2rem] flex items-center gap-xs pt-xs pb-xs pl-md pr-md cursor-pointer bg-surface-primary hover:bg-point-lavender"
+      onClick={() => handleOnClick(text)}
+    >
+      {icon && icon}
+      <Text size={14} color="text-primary">
+        {text}
+      </Text>
+    </li>
+  )
+}
+
+export default DropdownItem
