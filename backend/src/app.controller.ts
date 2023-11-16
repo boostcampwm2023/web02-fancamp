ef6313409fc4369f0e0f23a7a9d882e04143ec07
Jiwon-Heo <58136348+hunnypooh@users.noreply.github.com>
2023-11-16 22:40:44 +0900

    BGP-35, BGP-112 feat-be: 소켓 개발 환경 세팅
    1. 소켓 라이브러리 설치
    2. 정적파일 서빙
    3. index.html
    
    Co-authored-by: jhhwang <ttobe@users.noreply.github.com>

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //TODO: 클라이언트 연결되면 삭제 ->
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getIndex(@Res() res: Response) {
+    // index.html이 static 폴더에 위치한다면
+    return res.sendFile(join(__dirname, '..', 'static', 'index.html'));
   }
+  //<-
 }
