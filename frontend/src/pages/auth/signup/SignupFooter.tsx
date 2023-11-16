b73f466931882dcc9b441237cacd6c225959e5e2
Yeong <yjo0723@hotmail.com>
2023-11-16 17:28:02 +0900

    fix-fe: 회원가입 페이지 로그인 링크 수정

--- frontend/src/pages/auth/signup/SignupFooter.tsx
+++ frontend/src/pages/auth/signup/SignupFooter.tsx
@@ -4,7 +4,7 @@ export default function SignupFooter() {
   return (
     <div className='flex justify-center align-center gap-xs p-[2rem] border display-regular-14'>
       <span>계정이 있으신가요? </span>
-      <Link className='text-point-blue ml-2' to='/auth/signup'>
+      <Link className='text-point-blue ml-2' to='/auth/signin'>
         로그인
       </Link>
     </div>
