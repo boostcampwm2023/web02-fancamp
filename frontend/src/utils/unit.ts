4378e52baf891c6245e4eceb455132a86fc77c57
곽범석 <qjatjr99@naver.com>
2023-11-20 23:55:21 +0900

    feat-fe: 단위 관련 유틸 함수 추가

--- frontend/src/utils/unit.ts
+++ frontend/src/utils/unit.ts
@@ -2,3 +2,13 @@ export const pxToRem = (px: number) => {
   const rem = px / 16;
   return rem;
 };
+
+export const numberToString = (number: number) => {
+  if (number >= 1000000) {
+    return `${(number / 1000000).toFixed(2)}m`;
+  }
+  if (number >= 1000) {
+    return `${(number / 1000).toFixed(1)}k`;
+  }
+  return number.toString();
+};
