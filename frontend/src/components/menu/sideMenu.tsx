aa6ea43d0758714694db6b5d2607406bf57e761f
곽범석 <qjatjr99@naver.com>
2023-11-20 10:38:23 +0900

    fix-fe: Side Menu 컴포넌트 경로 수정

--- frontend/src/components/menu/sideMenu.tsx
+++ frontend/src/components/menu/sideMenu.tsx
@@ -7,24 +7,24 @@ interface SideMenuLinkProps {
   currentPath: string;
 }
 
-const SideMenu = () => {
+function SideMenu() {
   const location = useLocation();
 
   return (
     <div className="sticky left-[0] top-[0] z-10 flex h-[100vh] w-[12.5rem] flex-col">
-      <Link to={'/'}>
+      <Link to="/">
         <img src="/src/assets/icons/logo.png" alt="메인 로고" />
       </Link>
       <Hr />
       <div className="flex flex-col gap-sm pb-sm pt-sm">
-        <SideMenuLink to={'/'} text="Home" currentPath={location.pathname} />
+        <SideMenuLink to="/" text="Home" currentPath={location.pathname} />
         <SideMenuLink
-          to={'/search'}
+          to="/search"
           text="Search"
           currentPath={location.pathname}
         />
         <SideMenuLink
-          to={'/explore'}
+          to="/explore"
           text="Explore"
           currentPath={location.pathname}
         />
@@ -32,41 +32,46 @@ const SideMenu = () => {
       <Hr />
       <div className="flex flex-col gap-sm pb-sm pt-sm">
         <SideMenuLink
-          to={'/auth/signin'}
+          to="/auth/signin"
           text="로그인"
           currentPath={location.pathname}
         />
         <SideMenuLink
-          to={'/auth/signup'}
+          to="/auth/signup"
           text="회원가입"
           currentPath={location.pathname}
         />
         <SideMenuLink
-          to={'/camps/0b5060ce-dfb4-4497-b0bf-34c6b7fce368/'}
+          to="/camps/0b5060ce-dfb4-4497-b0bf-34c6b7fce368/post"
           text="캠프"
           currentPath={location.pathname}
         />
         <SideMenuLink
-          to={'/camps/0b5060ce-dfb4-4497-b0bf-34c6b7fce368/chat'}
+          to="/camps/0b5060ce-dfb4-4497-b0bf-34c6b7fce368/chat"
           text="> 채팅"
           currentPath={location.pathname}
         />
         <SideMenuLink
-          to={'/camps/0b5060ce-dfb4-4497-b0bf-34c6b7fce368/post'}
+          to="/camps/0b5060ce-dfb4-4497-b0bf-34c6b7fce368/post"
           text="> 포스트"
           currentPath={location.pathname}
         />
         <SideMenuLink
-          to={'/components/demo'}
+          to="/demo/components"
           text="컴포넌트 데모"
+          currentPath={location.pathname}
+        />
+        <SideMenuLink
+          to="demo/api/rest"
+          text="Mock Api"
           currentPath={location.pathname}
         />
       </div>
     </div>
   );
-};
+}
 
-const SideMenuLink = ({ to, text, currentPath }: SideMenuLinkProps) => {
+function SideMenuLink({ to, text, currentPath }: SideMenuLinkProps) {
   return (
     <Link to={to} className="flex pb-sm pl-md pr-md pt-sm">
       <Text
@@ -77,10 +82,10 @@ const SideMenuLink = ({ to, text, currentPath }: SideMenuLinkProps) => {
       </Text>
     </Link>
   );
-};
+}
 
-const Hr = () => {
+function Hr() {
   return <hr className="h-[0.0625rem] border-0 bg-contour-primary" />;
-};
+}
 
 export default SideMenu;
