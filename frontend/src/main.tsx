12f789d68561e2673593803bf2fb54c03f813327
곽범석 <qjatjr99@naver.com>
2023-11-21 00:00:10 +0900

    fix-fe: QueryClient export 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,7 +15,7 @@ async function enableMocking() {
   return worker.start();
 }
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
