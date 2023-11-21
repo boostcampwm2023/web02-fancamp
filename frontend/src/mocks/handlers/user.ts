4c3b5d1666692a2033c9abdb58468ef46e641ce6
곽범석 <qjatjr99@naver.com>
2023-11-21 20:40:39 +0900

    feat-fe: mock 서버 유저 정보 업데이트 기능 추가

--- frontend/src/mocks/handlers/user.ts
+++ frontend/src/mocks/handlers/user.ts
@@ -1,5 +1,6 @@
-import { HttpResponse, http } from 'msw';
+import { HttpResponse, delay, http } from 'msw';
 import UserInfo from '../dummy/userInfo.json';
+import { CampEditable } from '../../types/api/camp';
 
 export const handlers = [
   // 특정 유저 정보 가져오기
@@ -7,5 +8,16 @@ export const handlers = [
     const { userId } = params;
     const user = UserInfo.find((u) => u.userId === userId);
     return HttpResponse.json(user);
+  }),
+  // 특정 유저 정보 업데이트
+  http.patch('/api/users/:userId', async ({ request, params }) => {
+    await delay(1000);
+    const { userId } = params;
+    const { userName } = (await request.json()) as Partial<CampEditable>;
+    const userIndex = UserInfo.findIndex((u) => u.userId === userId);
+    if (userName !== undefined) {
+      UserInfo[userIndex].userName = userName;
+    }
+    return HttpResponse.json(UserInfo[userIndex]);
   }),
 ];
