3d35ad62ae1208464523ed55e0b603d602c7df8f
곽범석 <qjatjr99@naver.com>
2023-11-23 00:02:51 +0900

    fix-fe: input 컴포넌트 스타일 수정

--- frontend/src/components/input/input.tsx
+++ frontend/src/components/input/input.tsx
@@ -1,18 +1,21 @@
+import Text from '../text/text';
+
 interface InputProps<State>
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   icon?: React.ReactNode;
+  errorMessage?: string;
   setValue?: React.Dispatch<React.SetStateAction<State>>;
 }
 
 const inputClassName =
   'placeholder:display-regular-14 flex-1 bg-transparent p-sm focus:outline-none ';
 const inputBorderClassName =
-  'smooth-transition mb-4 border-b-sm border-text-primary ' +
+  'smooth-transition border-b-sm border-text-primary ' +
   'group-focus-within:border-b-sm group-focus-within:border-point-blue';
 
 function Input<State>(props: InputProps<State>) {
-  const { label, icon, setValue, ...inputProps } = props;
+  const { label, icon, errorMessage, setValue, ...inputProps } = props;
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (setValue) {
@@ -22,7 +25,7 @@ function Input<State>(props: InputProps<State>) {
   };
 
   return (
-    <div className="group flex w-full flex-col gap-sm">
+    <div className="group flex w-full flex-col gap-xs">
       {label && <span className="display-regular-14">{label}</span>}
       <div className="flex flex-row items-center">
         {icon && icon}
@@ -37,6 +40,11 @@ function Input<State>(props: InputProps<State>) {
         />
       </div>
       <div className={inputBorderClassName} />
+      <div className="flex h-[1rem] items-center justify-end">
+        <Text size={12} color="point-red" className="text-right">
+          {errorMessage}
+        </Text>
+      </div>
     </div>
   );
 }
