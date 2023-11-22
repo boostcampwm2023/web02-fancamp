94e3530c4c4c4d370bd894a4cd2638bc873bf609
곽범석 <qjatjr99@naver.com>
2023-11-23 00:06:28 +0900

    feat-fe: 로그인 페이지 구현

--- frontend/src/pages/auth/signin/index.tsx
+++ frontend/src/pages/auth/signin/index.tsx
@@ -1,11 +1,77 @@
+import { FormEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 import SigninForm from './SigninForm';
-import SigninFooter from './SigninFooter';
+import useAuth from '../../../hooks/useAuth';
+import { validateSign } from '../../../utils/validate';
+import { signin } from '../../../api/auth';
+
+interface SignMutateStatus {
+  isPending: boolean;
+  isError: boolean;
+  isSuccess: boolean;
+}
 
 export default function SigninPage() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const [signMutateStatus, setSignMutateStatus] = useState<SignMutateStatus>({
+    isPending: false,
+    isError: false,
+    isSuccess: false,
+  });
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+
+  const signinMutation = useMutation({
+    mutationFn: () => signin(email, password),
+    onSuccess: (data) => {
+      setAuth(data);
+      navigate('/');
+    },
+  });
+
+  useEffect(() => {
+    if (auth) {
+      navigate('/error', {
+        state: { error: '이미 로그인 하셨어요! 😉' },
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    setSignMutateStatus({
+      isPending: signinMutation.isPending,
+      isError: signinMutation.isError,
+      isSuccess: signinMutation.isSuccess,
+    });
+  }, [
+    signinMutation.isPending,
+    signinMutation.isError,
+    signinMutation.isSuccess,
+  ]);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const validateEmail = validateSign.isEmailOk(email);
+    const validatePassword = validateSign.isPasswordOk(password);
+    if (!validateEmail || !validatePassword) {
+      return;
+    }
+    signinMutation.mutate();
+  };
+
   return (
-    <div className="h-[600px] w-[500px] p-8">
-      <SigninForm />
-      <SigninFooter />
+    <div className="h-[100vh] w-full  bg-[#F4F0E1]">
+      <SigninForm
+        email={email}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+        status={signMutateStatus}
+      />
     </div>
   );
 }
