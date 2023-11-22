b01a411fc123ef67a73bd1bc9fe7aac1f85c9c43
곽범석 <qjatjr99@naver.com>
2023-11-23 00:06:51 +0900

    feat-fe: auth 관련 검증 함수 추가

---
+++ frontend/src/utils/validate.ts
@@ -0,0 +1,11 @@
+export const validateSign = {
+  isEmailOk: (email: string) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z]{2,}$/;
+    const result = emailRegex.test(email);
+    return result;
+  },
+  isPasswordOk: (password: string) => {
+    const result = password.length >= 4 && password.length <= 20;
+    return result;
+  },
+};
