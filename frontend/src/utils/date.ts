65c11f63ee851469a12fe9e7410213c2e8f7cff2
곽범석 <qjatjr99@naver.com>
2023-11-23 22:03:30 +0900

    feat-fe: 날짜 관련 포멧 함수 구현

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -4,3 +4,34 @@ export function getLocaleString(): string {
     minute: '2-digit',
   });
 }
+
+export const formatDate = (date: string) => {
+  const currDate = new Date();
+  const currYear = currDate.getFullYear();
+  const currDay = currDate.getDate();
+
+  const inputDate = new Date(date);
+  const inputYear = inputDate.getFullYear();
+  const inputMonth = inputDate.getMonth() + 1;
+  const inputDay = inputDate.getDate();
+  const inputHour = inputDate.getHours();
+  const inputMinute = inputDate.getMinutes();
+
+  if (inputYear !== currYear) {
+    return `${inputYear}.${inputMonth.toString().padStart(2, '0')}.${inputDay
+      .toString()
+      .padStart(2, '0')} ${inputHour.toString().padStart(2, '0')}:${inputMinute
+      .toString()
+      .padStart(2, '0')}`;
+  }
+  if (inputDay !== currDay) {
+    return `${inputMonth.toString().padStart(2, '0')}.${inputDay
+      .toString()
+      .padStart(2, '0')} ${inputHour.toString().padStart(2, '0')}:${inputMinute
+      .toString()
+      .padStart(2, '0')}`;
+  }
+  return `${inputHour.toString().padStart(2, '0')}:${inputMinute
+    .toString()
+    .padStart(2, '0')}`;
+};
