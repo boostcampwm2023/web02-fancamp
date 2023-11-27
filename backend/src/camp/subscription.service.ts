f09f8cfaf0cdeee0b0e8503cbea15ebdd028ef58
TTobe <35289282+TTobe@users.noreply.github.com>
2023-11-27 16:11:49 +0900

    BGP-204 feat-be: 한 유저의 모든 구독여부 확인 및 하나의 캠프 구독 확인
    Co-authored-by: Jiwon-Heo <hunnypooh@users.noreply.github.com>

--- backend/src/camp/subscription.service.ts
+++ backend/src/camp/subscription.service.ts
@@ -1,23 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { SubscriptionRepository } from './subscription.repository';
 import { UserService } from 'src/user/user.service';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
+import { ERR_MESSAGE } from 'src/utils/constants';
+import { CampRepository } from './camp.repository';
 
 @Injectable()
 export class SubscriptionService {
   constructor(
     private readonly subscriptionRepository: SubscriptionRepository,
+    private readonly campRepository: CampRepository
   ) {}
   create(createSubscriptionDto: CreateSubscriptionDto) {
     return this.subscriptionRepository.createSubscription(
       createSubscriptionDto,
     );
   }
-  findOne(camperId: number, masterId: number) {
-    return this.subscriptionRepository.findOne(camperId, masterId);
+  async findOne(camperId: number, masterId: number) {
+    if(!await this.subscriptionRepository.findOne(camperId, masterId)){
+      throw new HttpException(
+        ERR_MESSAGE.NOT_SUBSCRIBED,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    
+
   }
-  findAll(camperId: number){
-    return this.subscriptionRepository.findAll(camperId)
+  async findAll(camperId: number){
+    const subscriptions = await this.subscriptionRepository.findAll(camperId);    
+    return await Promise.all(
+      subscriptions.map(async (subscription) => {
+        console.log(subscription);
+        const camp = await this.campRepository.findOneByMasterId(subscription.masterId);
+        return camp;
+      }),
+    );
   }
 
 }
