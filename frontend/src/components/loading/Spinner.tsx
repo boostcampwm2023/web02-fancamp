c88692433b9f76924ba8832dbd7e257640fd9fae
곽범석 <qjatjr99@naver.com>
2023-11-30 01:18:53 +0900

    fix-fe: Spinner 컴포넌트 props 수정

--- frontend/src/components/loading/Spinner.tsx
+++ frontend/src/components/loading/Spinner.tsx
@@ -4,18 +4,19 @@ interface SpinnerProps {
   width?: number;
   height?: number;
   className?: string;
+  color?: string;
 }
 
-function Spinner({ width, height, className }: SpinnerProps) {
+function Spinner({ width, height, className, color }: SpinnerProps) {
   return (
     <TailSpin
       height={height || '32'}
       width={width || '32'}
-      color="#0087E9"
+      color={color || '#0087E9'}
       ariaLabel="loading-spinner"
       radius="1"
       wrapperStyle={{}}
-      wrapperClass={`relative w-fit ${className || ''}}`}
+      wrapperClass={`relative w-fit ${className || ''}`}
       visible
     />
   );
