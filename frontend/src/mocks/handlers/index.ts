f920bd91f03c7041720a1ae49e92186554016458
곽범석 <qjatjr99@naver.com>
2023-11-30 01:27:23 +0900

    feat-fe: mock 서버 채팅 기능 추가

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import { handlers as postHandlers } from './post';
 import { handlers as commentHandlers } from './comment';
 import { handlers as userHandlers } from './user';
+import { handlers as chatrHandlers } from './chat';
 
-export const handlers = [...postHandlers, ...commentHandlers, ...userHandlers];
+export const handlers = [
+  ...postHandlers,
+  ...commentHandlers,
+  ...userHandlers,
+  ...chatrHandlers,
+];
