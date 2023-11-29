2d8a92bae1836420d8a33d34f731464301cb3e47
곽범석 <qjatjr99@naver.com>
2023-11-30 02:25:28 +0900

    feat-fe: 캠프의 포스트를 가져오는 쿼리 추가

--- frontend/src/hooks/api/useCampQuery.ts
+++ frontend/src/hooks/api/useCampQuery.ts
@@ -1,5 +1,6 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { Camp } from '@type/api/camp';
+import { Post } from '@type/api/post';
 import useFetch from './useFetch';
 
 export const getCampQuery = (campId: string) => {
@@ -7,6 +8,21 @@ export const getCampQuery = (campId: string) => {
     queryKey: ['camp', campId],
     queryFn: () =>
       useFetch(`/api/camps/${campId}`, {
+        method: 'GET',
+        credentials: 'include',
+      }),
+    gcTime: 0,
+    staleTime: 0,
+  });
+
+  return { data, isError, isLoading };
+};
+
+export const getCampPostsQuery = (campId: string) => {
+  const { data, isError, isLoading } = useSuspenseQuery<Post[]>({
+    queryKey: ['posts', campId],
+    queryFn: () =>
+      useFetch(`/api/posts/camps/${campId}`, {
         method: 'GET',
         credentials: 'include',
       }),
