b5fee5de7608ee04daee12ba28bffb891808d31d
곽범석 <qjatjr99@naver.com>
2023-12-05 00:34:37 +0900

    feat-fe: Camp 카드 컴포넌트 추가

---
+++ frontend/src/components/card/Card.tsx
@@ -0,0 +1,24 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+}
+
+const buttonClassName =
+  'display-regular-14 border-sm shadow-[0px_0px_0px_rgba(0,0,0)] ' +
+  '[&:hover]:mt-[-2px] [&:hover]:ml-[-2px] [&:hover]:shadow-[4px_4px_0px_rgba(0,0,0)] ' +
+  'transition-all duration-200 ';
+
+function Card(props: ButtonProps) {
+  const { children, className, ...buttonProps } = props;
+
+  return (
+    <button
+      {...buttonProps}
+      type="button"
+      className={buttonClassName + (className || '')}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Card;
