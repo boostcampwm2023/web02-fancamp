7ac5071785ec25bc75f469039c63c0df1c576de8
곽범석 <qjatjr99@naver.com>
2023-12-06 10:38:56 +0900

    fix-fe: 검색 기능 수정

--- frontend/src/components/input/SearchInput.tsx
+++ frontend/src/components/input/SearchInput.tsx
@@ -7,12 +7,13 @@ interface SearchInputProps {
 
 function SearchInput({ value, setValue }: SearchInputProps) {
   return (
-    <div className="flex w-full items-center gap-md border-sm border-text-primary pb-sm pl-md pr-md pt-sm">
-      <SearchIcon width={16} />
+    <div className="flex h-[3rem] w-full items-center gap-md border-sm border-text-primary pb-sm pl-md pr-md pt-sm">
+      <SearchIcon width={20} />
       <input
         type="text"
         className="h-full w-full bg-transparent outline-none display-regular-16"
         value={value}
+        placeholder="마음에 드는 캠프를 찾아보세요"
         onChange={(e) => setValue(e.currentTarget.value)}
       />
     </div>
