0e6ea0deb965b67a30277e4b02eec906f3e8704c
Yeong Oh <yjo0723@hotmail.com>
2023-12-07 20:17:32 +0900

    refactor-fe: 불필요한 파일들 제거

--- frontend/src/mocks/handlers/index.ts
+++
@@ -1,5 +0,0 @@
-import { handlers as postHandlers } from './post';
-import { handlers as commentHandlers } from './comment';
-import { handlers as userHandlers } from './user';
-
-export const handlers = [...postHandlers, ...commentHandlers, ...userHandlers];
